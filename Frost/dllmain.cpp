088cb99e38d2637fb6afeccd8b936376099e331b
Tozic Gaming <isameldein2012@gmail.com>
2024-04-07 13:23:41 +0400

    Added InitializeClient function

--- Frost/dllmain.cpp
+++ Frost/dllmain.cpp
@@ -1,14 +1,25 @@
 // dllmain.cpp : Defines the entry point for the DLL application.
 
 #pragma region Includes
+
 // C++ Includes
 #include <Windows.h>
+
+#pragma endregion
+
+// This function initializes the client.
+void InitializeClient() { // A method called "InitializeClient" to initialize hooks/modules etc.
+
+}
 
 BOOL APIENTRY DllMain(HMODULE module, DWORD  reason, LPVOID reserved)
 {
     if (reason == DLL_PROCESS_ATTACH) {
         // Prevent the DLL from being unloaded when the DLL is first attached to the process
         DisableThreadLibraryCalls(module); // Give it the module of the dll.
+
+        // Create a new thread to initialize the client on using the CreatThread function C++ provides
+        CreateThread(nullptr, 0, (LPTHREAD_START_ROUTINE)InitializeClient, module, 0, nullptr);
     }
     return TRUE;
 }
