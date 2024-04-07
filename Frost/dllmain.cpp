fa9c2d5f6b561e187f1f2002844842372aae25c1
Tozic Gaming <isameldein2012@gmail.com>
2024-04-07 13:16:07 +0400

    Renamed Parameters

--- Frost/dllmain.cpp
+++ Frost/dllmain.cpp
@@ -4,12 +4,9 @@
 // C++ Includes
 #include <Windows.h>
 
-BOOL APIENTRY DllMain( HMODULE hModule,
-                       DWORD  ul_reason_for_call,
-                       LPVOID lpReserved
-                     )
+BOOL APIENTRY DllMain(HMODULE module, DWORD  reason, LPVOID reserved)
 {
-    switch (ul_reason_for_call)
+    switch (reason)
     {
     case DLL_PROCESS_ATTACH:
     case DLL_THREAD_ATTACH:
